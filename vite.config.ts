66228af8de98836535e2fd19df002508c5e3dfab
Emma-Hyejin <cjd191058@naver.com>
2023-11-13 23:04:40 +0900

    Chore: 환경 세팅 TS + TailwindCSS + Vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import macrosPlugin from 'vite-plugin-babel-macros';
+import * as path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    macrosPlugin(),
+    react(),
+  ],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  base: '/',
+})
